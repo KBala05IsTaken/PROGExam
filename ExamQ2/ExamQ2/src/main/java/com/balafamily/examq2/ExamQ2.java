e8e5141db38a979f07efd5724542e5305e516853
KBala05IsTaken <ST10474385@rcconnect.edu.za>
2025-11-10 06:43:07 +0200

    Update ExamQ2.java
    Updating the project

--- ExamQ2/ExamQ2/src/main/java/com/balafamily/examq2/ExamQ2.java
+++ ExamQ2/ExamQ2/src/main/java/com/balafamily/examq2/ExamQ2.java
@@ -1,7 +1,10 @@
 package com.balafamily.examq2;
-public class ExamQ2 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.util.Scanner;
+public class ExamQ2 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scnStr = new Scanner(System.in);
     }
 }
