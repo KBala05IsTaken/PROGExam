e6529be3c0e32920e048f866b5b55d5afe81df42
KBala05IsTaken <ST10474385@rcconnect.edu.za>
2025-11-10 06:33:23 +0200

    Started with this

---
+++ ExamQ1/ExamQ1/src/main/java/com/balafamily/examq1/ExamQ1.java
@@ -0,0 +1,9 @@
+package com.balafamily.examq1;
+import java.util.Scanner;
+public class ExamQ1 
+{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ExamQ2/ExamQ2/src/main/java/com/balafamily/examq2/ExamQ2.java
@@ -0,0 +1,7 @@
+package com.balafamily.examq2;
+public class ExamQ2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
